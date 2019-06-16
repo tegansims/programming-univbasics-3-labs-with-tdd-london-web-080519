f3201e91b27f5b825cbb75f286cc1406225912b1
Tegan Sims <tegansims@hotmail.com>
2019-06-16 15:22:31 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,3 +5,7 @@ second_number = 6
 sum = first_number + second_number
 
 difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
